f6d5900260dd3b6ea80fe8ec35af7fc35d33302a
Wellington <wellington.vargas.bernardes80@gmail.com>
2026-05-21 12:00:25 -0300

    Mapeando arquivo temporário para resetar o case

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { DEVELOPER, scrollTo } from "../data/developerData";
-import CodeCard from "../components/codeCard";
+import CodeCard from "../components/CodeCard";
 
 export default function HeroSection() {
   return (
